879df89374dbd0249e13df4d86727b914f671796
Mlotshwa <mlotshwadineo080@gmail.com>
2023-10-05 23:40:18 -0700

    updated

---
+++ task_0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Description:Print a statement that explains what we are doing
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        printf("Heloo, we are testing Betty style\n");
+
+        return (0);
+}
